2158f3b7dfd5e01f993adecb4b51d86d7a6c2a71
Kapil <kingranikapil3@gmail.com>
2024-01-27 13:12:29 +0530

    Feat: 2 Clients Can Communicate in Sync

--- Client.java
+++ Client.java
@@ -1,6 +1,28 @@
 import java.io.*;
 import java.net.*;
 
+class HandleResponse extends Thread {
+    BufferedReader serverIn;
+    String line;
+
+    HandleResponse(BufferedReader serverIn, String line) {
+        this.serverIn = serverIn;
+        this.line = line;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            while (!"exit".equalsIgnoreCase(line = serverIn.readLine())) {
+                System.out.println();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 9876);
@@ -8,14 +30,27 @@ class Client {
         PrintWriter serverOut = new PrintWriter(socket.getOutputStream(), true);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
 
-        // until i write exit take my input
+        System.out.println("Enter UserName:");
+        serverOut.println(br.readLine());
+        String serverResponse = serverIn.readLine();// greetings from server
+        System.out.println(serverResponse);
+
+        System.out.println("wants to chat with ?");
+        serverOut.println(br.readLine());
+        serverResponse = serverIn.readLine();
+        System.out.println(serverResponse);
+
+        serverResponse = serverIn.readLine();
+        System.out.println(serverResponse); // user is available or not that response will be printed...
+
+        System.out.println("Write messages and if u want to quit type 'exit' ");
+        String line = "";
+        new HandleResponse(serverIn, line);// Response Thread
+
+        // sending Thread
         while (!"exit".equalsIgnoreCase(line = br.readLine())) {
             serverOut.println(line);
-            String response = serverIn.readLine();
-            System.out.println("Client Got this response " + response);
-
         }
         serverOut.println("exit");
         socket.close();
--- Echoer.java
+++ Echoer.java
@@ -1,40 +1,76 @@
+
+/**
+ *      The Server OutputStream Will Become Client InputStream
+        The Server InputStream will Become Client OutputStream
+        (vice versa)
+
+ *      In Java, a PrintStream buffers data before sending it to its destination,
+        like a file or network. Enabling auto-flush (true in constructor) makes it
+        automatically flush the buffer, ensuring data is promptly sent, especially
+        after writing a newline....
+      
+ */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Echoer extends Thread {
     private Socket clientSocket;
+    private static Map<String, Socket> db = new HashMap<>();// db-Static so that values common for all clients
+    BufferedReader clientIn;
+    PrintWriter clientOut;
+    String line;
 
     public Echoer(Socket clientSocket) {
         this.clientSocket = clientSocket;
         this.start();
     }
 
-    // Keep In Mind
-    // The Server OutputStream Will Become Client InputStream
-    // The Server InputStream will Become Client OutputStream
+    public void registerUser(String username, Socket clientSocket) {
+        db.put(username, clientSocket);
+    }
+
+    public boolean findUser(String username) {
+        if (db.containsKey(username)) {
+            clientOut.println(username + " is online ");
+            return true;
+        } else {
+            clientOut.println(username + " is offline or Might Be He is not on our Chat Application System");
+            return false;
+        }
+    }
 
     public void run() {
         try {
-            BufferedReader clientIn = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
 
-            // In Java, a PrintStream buffers data before sending it to its destination,
-            // like a file or network. Enabling auto-flush (true in constructor) makes it
-            // automatically flush the buffer, ensuring data is promptly sent, especially
-            // after writing a newline
-            PrintWriter clientOut = new PrintWriter(clientSocket.getOutputStream(), true);
-            String line;
+            clientIn = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
+            clientOut = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            // until i got exit from the client i need to print
-            while (!"exit".equalsIgnoreCase(line = clientIn.readLine())) {
-                System.out.println("Server Got " + line);
-                clientOut.println("You Sent me " + line);
+            String username = clientIn.readLine();
+            clientOut.println("Welcome To Chat Application " + username);
 
+            registerUser(username, clientSocket); // adding this to our hashmap
+
+            String receiverUsername = clientIn.readLine();
+            clientOut.println("Hold On Establishing Connection Between " + username + "and" + receiverUsername);
+
+            boolean result = findUser(receiverUsername); // finding the user in our db
+
+            if (result == true) {
+                Socket receiverSocket = db.get(receiverUsername);
+                PrintWriter receiverOut = new PrintWriter(receiverSocket.getOutputStream(), true);
+
+                while (!"exit".equalsIgnoreCase(line = clientIn.readLine())) {
+                    receiverOut.println(username + ":-" + line);
+                }
+                System.out.println("Client Closed");
+
+                clientSocket.close();
             }
-            System.out.println("Client Closed");
 
-            clientSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
